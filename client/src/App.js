15e6d38a19ee66d7c29f93bf35fdbcb27377da85
CB_ATL_117_William_Fleming <cb_atl_117_william_fleming@CBs-MacBook-Air.local>
2018-11-13 13:30:31 -0500

    imported browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, and Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
-        
+        <Switch>
+          
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
