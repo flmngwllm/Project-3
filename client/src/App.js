fb03e72fda51c3d451406ceef35094c59537d09c
CB_ATL_117_William_Fleming <cb_atl_117_william_fleming@CBs-MacBook-Air.local>
2018-11-13 13:32:00 -0500

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, and Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -7,7 +7,7 @@ class App extends Component {
       <Router>
       <div>
         <Switch>
-          
+
         </Switch>
       </div>
       </Router>
